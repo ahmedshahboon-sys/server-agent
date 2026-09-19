5d647a7601c4043ff2e9c7483ed0c3182e4645ea
ahmedshahboon-sys <hacklibyano85@gmail.com>
2026-09-19 22:40:09 +0200

    Group 7: validate migration 8 exactly once

--- tests/database/sqlite.test.ts
+++ tests/database/sqlite.test.ts
@@ -7,8 +7,9 @@ describe('SqliteDatabase', () => {
     const db = new SqliteDatabase(':memory:');
     try {
       const versions = db.raw.prepare('SELECT version FROM schema_migrations ORDER BY version').all();
-      assert.deepEqual(versions.map((row) => ({ ...row })), [{ version: 1 }, { version: 2 }, { version: 3 }, { version: 4 }, { version: 5 }, { version: 6 }, { version: 7 }]);
+      assert.deepEqual(versions.map((row) => ({ ...row })), [{ version: 1 }, { version: 2 }, { version: 3 }, { version: 4 }, { version: 5 }, { version: 6 }, { version: 7 }, { version: 8 }]);
       assert.doesNotThrow(() => db.raw.exec('SELECT archived_at FROM projects;'));
+      assert.doesNotThrow(() => db.raw.exec('SELECT cancel_requested_at FROM persistent_operations;'));
       assert.doesNotThrow(() => db.raw.exec('SELECT * FROM tasks; SELECT * FROM jobs; SELECT * FROM idempotency_keys; SELECT * FROM deployments; SELECT * FROM health_checks; SELECT * FROM database_audit; SELECT * FROM recovery_runs; SELECT * FROM rollback_plans; SELECT * FROM mcp_audit; SELECT * FROM persistent_operations; SELECT * FROM auth_principals; SELECT * FROM auth_credentials; SELECT * FROM auth_audit;'));
     } finally { db.close(); }
   });
